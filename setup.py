21a16b3a54e5a825391964d660766fb4bd78a9b1
dliu936 <dli936@g.harvard.edu>
2019-12-12 18:40:50 -0500

    Fixed issue with installation
    Moved the files to correct location

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup
 setup(
     name='oandareports',
     version='0.1.0',
-    packages=['tests', 'reports', 'examples', 'helperfiles'],
-    url='https://github.com/dliu936/oandareports.git',
+    packages=['oandareports', 'oandareports.tests', 'oandareports.reports', 'oandareports.examples', 'oandareports.helperfiles'],
+    url='https://github.com/oeyvindds/oandareports.git',
     license='MIT',
     author='Oyvind Sporck & Diana Liu',
     author_email='',
